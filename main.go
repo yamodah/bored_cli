c4b00a2247aaa34ceb6b19c0c6b00acf52da1e39
yamodah <yassinedahlek@gmail.com>
2022-02-19 15:29:34 -0500

    takes input replies with welcome message

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("bismillah")
+	var name string
+	// var time uint
+	// var break bool
+
+	fmt.Println("What is your name ?")
+	fmt.Scan(&name)
+	fmt.Printf("Welcome %v \n", name)
 }

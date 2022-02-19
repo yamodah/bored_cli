ffcba52b44e7430883397d9daea949706979a46d
yamodah <yassinedahlek@gmail.com>
2022-02-19 16:23:20 -0500

    works but is not fully random need to seed new numbers

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	var name string
 	var quick_break string
+	rand.Seed(time.Now().UnixNano())
 	options := map[string][]string{
 		"yes": {"Go watch some TikToks",
 			"Watch a Youtube video",
@@ -12,7 +17,7 @@ func main() {
 			"Find someone to bother for a few minutes",
 			"Grab a quick snack",
 			"Go juggle a soccer ball for a bit",
-			"Oops sorry, no break for you",
+			"Go back to work, sorry no break for you",
 			"Take some deep breathes and recenter yourself"},
 		"no": {"Catch up on soccer highlights",
 			"Watch some anime",
@@ -21,12 +26,14 @@ func main() {
 			"Learn about a new coding language/framework",
 			"Refactor code from previous projects",
 			"Start a new project",
-			"Reach out to someone you miss"},
+			"Reach out to someone you miss",
+			"Take a nap"},
 	}
 	fmt.Println("What is your name ?")
 	fmt.Scan(&name)
 	fmt.Printf("Welcome %v, let's help you choose an activity \n", name)
 	fmt.Println("Is this a quick break ? (yes or no) ")
 	fmt.Scan(&quick_break)
-
+	activity := options[quick_break][rand.Intn(7)]
+	fmt.Printf("To defeat your boredom you should ... \n %v\n", activity)
 }

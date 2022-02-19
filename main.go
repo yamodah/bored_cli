634af8091e3cfe0877cfabbebc1976481597a30f
yamodah <yassinedahlek@gmail.com>
2022-02-19 15:49:21 -0500

    removed time var and added options map

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	var name string
-	var time uint
 	var quick_break string
-
+	options := map[string][]string{
+		"yes": {},
+		"no":  {},
+	}
 	fmt.Println("What is your name ?")
 	fmt.Scan(&name)
 	fmt.Printf("Welcome %v \n", name)
-	fmt.Println("How much time do you have (in min)?")
-	fmt.Scan(&time)
 	fmt.Println("Is this a break yes or no ?")
 	fmt.Scan(&quick_break)
 

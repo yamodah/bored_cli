f17424b0c8dc8d52ad6353cee26919ee1814a5d8
yamodah <yassinedahlek@gmail.com>
2022-02-19 15:41:43 -0500

    added pointer to allow scanning tot he variables

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	var name string
-	// var time uint
-	// var break bool
+	var time uint
+	var quick_break string
 
 	fmt.Println("What is your name ?")
 	fmt.Scan(&name)
 	fmt.Printf("Welcome %v \n", name)
+	fmt.Println("How much time do you have (in min)?")
+	fmt.Scan(&time)
+	fmt.Println("Is this a break yes or no ?")
+	fmt.Scan(&quick_break)
+
 }
